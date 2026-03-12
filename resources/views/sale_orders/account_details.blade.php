5a54f13b2570008968f6668cb59087361001fb15
falgunioctacore <falgunioctacore@gmail.com>
2026-03-12 14:55:19 +0530

    small bug fixed

--- resources/views/sale_orders/account_details.blade.php
+++ resources/views/sale_orders/account_details.blade.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Customer Account Details</title>
 
     <style>
-
         @page {
             margin: 120px 40px 60px 40px;
         }
@@ -23,11 +23,11 @@
             height: 80px;
         }
 
-        .logo{
+        .logo {
             text-align: right;
         }
 
-        .title{
+        .title {
             text-align: center;
             font-size: 20px;
             font-weight: bold;
@@ -39,7 +39,8 @@
             border-collapse: collapse;
         }
 
-        th, td {
+        th,
+        td {
             border: 1px solid #000;
             padding: 7px;
             vertical-align: top;
@@ -51,177 +52,174 @@
             text-align: left;
         }
 
-        td{
+        td {
             width: 55%;
         }
 
-        .signature{
+        .signature {
             margin-top: 60px;
         }
 
-        .signature td{
+        .signature td {
             border: none;
         }
-
     </style>
 </head>
 
 <body>
 
-<header>
-    <div class="logo">
-        <img src="{{ public_path('/image/mr_logo.png') }}" height="70">
+    <header>
+        <div class="logo">
+            <img src="{{ public_path('/image/mr_logo.png') }}" height="70">
+        </div>
+    </header>
+
+    <div class="title">
+        Customer Account Details
     </div>
-</header>
 
-<div class="title">
-    Customer Account Details
-</div>
+    @php
 
-@php
+        $tax = $saleOrder->tax ?? 0;
 
-$tax=$saleOrder->tax??0;
+        $taxAmount = (($saleOrder->total_amount + $saleOrder->transporation_charge) * $tax) / 100;
 
-$taxAmount=(($saleOrder->total_amount + $saleOrder->transporation_charge) * $tax) / 100;
+        $grandTotal = $saleOrder->total_amount + $taxAmount;
 
-$grandTotal=$saleOrder->total_amount + $taxAmount;
+        $dicountType = $saleOrder->discount_type;
+        $unitPrice = $saleOrder->total_amount;
 
-$dicountType = $saleOrder->discount_type;
-$unitPrice=$saleOrder->total_amount;
+        if ($dicountType == 'percentage') {
+            $unitPrice = $unitPrice - ($unitPrice * $saleOrder->discount_percentage) / 100;
+        } elseif ($dicountType == 'amount') {
+            $unitPrice = $unitPrice - $saleOrder->disount_amount;
+        }
 
-if($dicountType == 'percentage'){
-$unitPrice = $unitPrice - ($unitPrice*$saleOrder->discount_percentage/100);
-}
-else if($dicountType == 'amount'){
-$unitPrice = $unitPrice - $saleOrder->disount_amount;
-}
-
-@endphp
+    @endphp
 
 
-<table>
+    <table>
 
-<tbody>
+        <tbody>
 
-<tr>
-<th>1. Company Name & Address</th>
-<td>
-{{ $saleOrder->quotation->customer->company_name ?? 'N/A' }} <br>
-{{ $saleOrder->address ?? 'N/A' }}
-</td>
-</tr>
+            <tr>
+                <th>1. Company Name & Address</th>
+                <td>
+                    {{ $saleOrder->quotation->customer->company_name ?? 'N/A' }} <br>
+                    {{ $saleOrder->address ?? 'N/A' }}
+                </td>
+            </tr>
 
-<tr>
-<th>2. Contact Person</th>
-<td>{{ $saleOrder->quotation->customer->contact_person_1_name ?? 'N/A' }}</td>
-</tr>
+            <tr>
+                <th>2. Contact Person</th>
+                <td>{{ $saleOrder->quotation->customer->contact_person_1_name ?? 'N/A' }}</td>
+            </tr>
 
-<tr>
-<th>3. Contact Number</th>
-<td>{{ $saleOrder->quotation->customer->contact_no ?? 'N/A' }}</td>
-</tr>
+            <tr>
+                <th>3. Contact Number</th>
+                <td>{{ $saleOrder->quotation->customer->contact_no ?? 'N/A' }}</td>
+            </tr>
 
-<tr>
-<th>4. Machine Name</th>
-<td>{{ $saleOrder->quotation->machine->name ?? 'N/A' }}</td>
-</tr>
+            <tr>
+                <th>4. Machine Name</th>
+                <td>{{ $saleOrder->quotation->machine->name ?? 'N/A' }}</td>
+            </tr>
 
-<tr>
-<th>5. Model No.</th>
-<td>{{ $saleOrder->quotation->modele->name ?? 'N/A' }}</td>
-</tr>
+            <tr>
+                <th>5. Model No.</th>
+                <td>{{ $saleOrder->quotation->modele->name ?? 'N/A' }}</td>
+            </tr>
 
-<tr>
-<th>6. Customer PO No.</th>
-<td>{{ $saleOrder->po_no ?? 'N/A' }}</td>
-</tr>
+            <tr>
+                <th>6. Customer PO No.</th>
+                <td>{{ $saleOrder->po_no ?? 'N/A' }}</td>
+            </tr>
 
-<tr>
-<th>7. PO Date Received by MR</th>
-<td>{{ formatDate($saleOrder->order_date??'') }}</td>
-</tr>
+            <tr>
+                <th>7. PO Date Received by MR</th>
+                <td>{{ formatDate($saleOrder->order_date ?? '') }}</td>
+            </tr>
 
-<tr>
-<th>8. Work Order No.</th>
-<td>{{ $saleOrder->work_order_no ?? 'N/A' }}</td>
-</tr>
+            <tr>
+                <th>8. Work Order No.</th>
+                <td>{{ $saleOrder->work_order_no ?? 'N/A' }}</td>
+            </tr>
 
-<tr>
-<th>9. Delivery Date</th>
-<td>{{ formatDate($saleOrder->delivery_date??'') }}</td>
-</tr>
+            <tr>
+                <th>9. Delivery Date</th>
+                <td>{{ formatDate($saleOrder->delivery_date ?? '') }}</td>
+            </tr>
 
-<tr>
-<th>10. Total Basic Price</th>
-<td>{{ format_indian_number($unitPrice ?? 0) }}</td>
-</tr>
+            <tr>
+                <th>10. Total Basic Price</th>
+                <td>{{ format_indian_number($unitPrice ?? 0) }}</td>
+            </tr>
 
-<tr>
-<th>11. Transportation</th>
-<td>{{ format_indian_number($saleOrder->transporation_charge ?? 0) }}</td>
-</tr>
+            <tr>
+                <th>11. Transportation</th>
+                <td>{{ format_indian_number($saleOrder->transporation_charge ?? 0) }}</td>
+            </tr>
 
-<tr>
-<th>12. Total GST Price</th>
-<td>{{ format_indian_number($taxAmount) }}</td>
-</tr>
+            <tr>
+                <th>12. Total GST Price</th>
+                <td>{{ format_indian_number($taxAmount) }}</td>
+            </tr>
 
-<tr>
-<th>13. Total Final Price</th>
-<td>{{ format_indian_number($saleOrder->grand_total ?? 0) }}</td>
-</tr>
+            <tr>
+                <th>13. Total Final Price</th>
+                <td>{{ format_indian_number($saleOrder->grand_total ?? 0) }}</td>
+            </tr>
 
-@php $index=14; @endphp
+            @php $index=14; @endphp
 
-@foreach ($saleOrder->payments as $i=>$payment)
+            @foreach ($saleOrder->payments as $i => $payment)
+                <tr>
+                    <th>{{ $index++ }}. Advance-{{ $i + 1 }}</th>
+                    <td>{{ format_indian_number($payment->amount ?? 0) }}</td>
+                </tr>
+            @endforeach
 
-<tr>
-<th>{{ $index++ }}. Advance-{{ $i+1 }}</th>
-<td>{{ format_indian_number($payment->amount ?? 0) }}</td>
-</tr>
+            <tr>
+                <th>{{ $index++ }}. Total Advance</th>
+                <td>{{ format_indian_number($saleOrder->advanace_payment ?? 0) }}</td>
+            </tr>
 
-@endforeach
+            <tr>
+                <th>{{ $index++ }}. Balance Payment</th>
+                <td>
+                    {{ format_indian_number(($saleOrder->grand_total ?? 0) - ($saleOrder->advanace_payment ?? 0)) }}
+                </td>
+            </tr>
 
-<tr>
-<th>{{ $index++ }}. Total Advance</th>
-<td>{{ format_indian_number($saleOrder->advanace_payment ?? 0) }}</td>
-</tr>
+            <tr>
+                <th>{{ $index++ }}. Remark</th>
+                <td>{{ $saleOrder->remark ?? '-' }}</td>
+            </tr>
 
-<tr>
-<th>{{ $index++ }}. Balance Payment</th>
-<td>
-{{ format_indian_number(($saleOrder->grand_total ?? 0) - ($saleOrder->advanace_payment ?? 0)) }}
-</td>
-</tr>
-
-<tr>
-<th>{{ $index++ }}. Remark</th>
-<td>{{ $saleOrder->remark ?? '-' }}</td>
-</tr>
-
-</tbody>
-</table>
+        </tbody>
+    </table>
 
 
-<table class="signature">
+    <table class="signature">
 
-<tr>
+        <tr>
 
-<td style="text-align:left;">
-<strong>Prepared By</strong>
-<br><br><br>
-____________________
-</td>
+            <td style="text-align:left;">
+                <strong>Prepared By</strong>
+                <br><br><br>
+                ____________________
+            </td>
 
-<td style="text-align:right;">
-<strong>Approved By</strong>
-<br><br><br>
-____________________
-</td>
+            <td style="text-align:right;">
+                <strong>Approved By</strong>
+                <br><br><br>
+                ____________________
+            </td>
 
-</tr>
+        </tr>
 
-</table>
+    </table>
 
 </body>
+
 </html>
