1f9fcc40da24bb592afaecbdbf0fc047378d7167
falgunioctacore <falgunioctacore@gmail.com>
2026-03-17 15:38:58 +0530

    vertical cooler mixer pdf handling

--- resources/views/components/technical-data.blade.php
+++ resources/views/components/technical-data.blade.php
@@ -18,8 +18,8 @@
         </div>
 
          <table class="parameter-table"
-            style="border-collapse: collapse; font-size: 14px; position: relative; left: 40px; width: 90%; line-height: 1.1;">
-            <tr><td style="padding: 8px; vertical-align: top; white-space: nowrap;">• &nbsp; Model</td><td class="value-cell">{{ $quotation->modele->name ?? '' }} </td></tr>
+            style="border-collapse: collapse; font-size: 14px; position: relative; left: 40px; width: 100%; line-height: 1.1;">
+            <tr><td style="padding: 8px; vertical-align: top; white-space: nowrap; width: 55%;">• &nbsp; Model</td><td class="value-cell">{{ $quotation->modele->name ?? '' }} </td></tr>
             <tr><td style="padding: 8px; vertical-align: top; white-space: nowrap;">• &nbsp; Product</td><td class="value-cell">{{ $quotation->application->name ?? '' }} Compound</td></tr>
 
             @if(isset($quotation->material_to_process))
@@ -59,9 +59,9 @@
         </div>
 
     <table class="parameter-table"
-            style="border-collapse: collapse; font-size: 14px; position: relative; left: 40px; width: 90%; line-height: 1.1;">
+            style="border-collapse: collapse; font-size: 14px; position: relative; left: 40px; width: 100%; line-height: 1.1;">
             <tr>
-                <td  style="padding: 8px; vertical-align: top; white-space: nowrap;">• &nbsp; Motor Requirement</td>
+                <td  style="padding: 8px; vertical-align: top; white-space: nowrap; width: 55%;">• &nbsp; Motor Requirement</td>
                 <td class="value-cell">{{ $quotation->motorRequirement->motor_requirement ?? '' }}</td>
             </tr>
             <tr>
@@ -71,12 +71,6 @@
             <tr>
                 <td style="padding: 8px; vertical-align: top; white-space: nowrap;">• &nbsp; Frequency</td>
                 <td class="value-cell">50Hz</td>
-            </tr>
-            <tr>
-                <td style="padding: 8px; vertical-align: top; white-space: nowrap;">• &nbsp; Control Panel</td>
-                <td class="value-cell">
-                    Complete Electrical Control Panel comprising of Thermocouple Wire, Digital Temperature Indicator with Ammeter & Voltmeter, Limit Switch & MCCB provided for safety precaution.
-                </td>
             </tr>
         </table>
     </div>
@@ -88,21 +82,27 @@
 <div class="page-break"  style="page-break-before: always; width: 100%;" >
     <div class="techincal-data parameter-table techincal-specification">
         <table class="parameter-table"
-            style="border-collapse: collapse; font-size: 14px; position: relative; left: 40px;top:60px; width: 90%; line-height: 1.1;">
+            style="border-collapse: collapse; font-size: 14px; position: relative; left: 40px;top:60px; width: 100%; line-height: 1.1;">
            <tr>
+                <td style="padding: 8px; vertical-align: top; white-space: nowrap;width:55%">• &nbsp; Control Panel</td>
+                <td class="value-cell">
+                    Complete Electrical Control Panel comprising of Thermocouple Wire, Digital Temperature Indicator with Ammeter & Voltmeter, Limit Switch & MCCB provided for safety precaution.
+                </td>
+            </tr>
+            <tr>
                 <td style="padding: 8px; vertical-align: top; white-space: nowrap;">• &nbsp; Mixing Tool</td>
                 <td class="value-cell">3 Tier Alloy Steel</td>
             </tr>
         </table>
         <h2></h2>
-         <div class="technical-data-sub-head" style="text-align: left;width: 95%;">
-             <h3 style="font-weight: bolder;">1.3 TRANSMISSION</h3>
-        </div>
+         <div class="technical-data-sub-head" style="text-align: left;width: 95%; margin-top:40px;">
+            <h3 style="font-weight: bold;">1.3  TRANSMISSION </h3>
+         </div>
 
         <table class="parameter-table"
-            style="border-collapse: collapse; font-size: 14px; position: relative; left: 40px; width: 90%; line-height: 1.1;">
+            style="border-collapse: collapse; font-size: 14px; position: relative; left: 40px; width: 100%; line-height: 1.1;">
             <tr>
-                <td style="padding: 8px; vertical-align: top; white-space: nowrap;">• &nbsp; Gear Box</td>
+                <td style="padding: 8px; vertical-align: top; white-space: nowrap; width: 55%;">• &nbsp; Gear Box</td>
                 <td class="value-cell">Heli Bevel Gear Box of Elecon Make</td>
             </tr>
             <tr>
